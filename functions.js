c190c8a5953a3e12f47dc15d21a1445cbdceacb5
bjorger <50590409+bjorger@users.noreply.github.com>
2020-01-08 06:21:37 +0100

    Added everything we need

--- functions.js
+++ functions.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
-const { dialog } = require('electron');
-const remote = require('electron').remote
+const { dialog } = require('electron').remote;
+const remote = require('electron').remote;
 
 let oldSearchTerm = '';
-
 
 let preview = document.getElementById('preview');
 let input = document.getElementById('input');
@@ -14,9 +13,11 @@ let fatty = document.getElementById('fatty');
 let cursive = document.getElementById('cursive');
 let search = document.getElementById('search');
 let replace = document.getElementById('replace');
+let line = document.getElementById('line');
+let link = document.getElementById('link');
+let email = document.getElementById('email');
 
 document.addEventListener('keyup', function(e) {
-
 	generateMarkdown();
 
 	if (e.ctrlKey && e.which === 72) {
@@ -33,14 +34,18 @@ document.addEventListener('keyup', function(e) {
 		let w = remote.getCurrentWindow();
 		w.close();
 	}
+
+	if(replace.value.length > 0 && search.value.length > 0 && e.keyCode === 13){
+		replaceFunction();
+	}
 });
 
 //https://stackoverflow.com/questions/52743841/find-and-highlight-word-in-text-using-js/52747318
 search.addEventListener('input', function() {
-	if(search.value.length > 0){
-		search.style.color = "#f3f3f3";
+	if (search.value.length > 0) {
+		search.style.color = '#f3f3f3';
 	}
-	
+
 	highlight();
 });
 
@@ -64,6 +69,44 @@ cursive.addEventListener('click', function() {
 	makeCursive();
 });
 
+line.addEventListener('click', function() {
+	var text = input.value;
+	var selectionStart = input.selectionStart;
+
+	var newText =
+		text.substring(0, selectionStart) +
+		'\n\n---\n\n' +
+		text.substring(selectionStart, text.length);
+	
+	input.value = newText;
+
+});
+
+link.addEventListener('click', function() {
+	var text = input.value;
+	var selectionStart = input.selectionStart;
+
+	var newText =
+		text.substring(0, selectionStart) +
+		'[Link-Text](http://www.google.at)' +
+		text.substring(selectionStart, text.length);
+
+	input.value = newText;
+});
+
+email.addEventListener('click', function() {
+	var text = input.value;
+	var selectionStart = input.selectionStart;
+
+	var newText =
+		text.substring(0, selectionStart) +
+		'<mail@example.com>' +
+		text.substring(selectionStart, text.length);
+	
+	input.value = newText;
+
+});
+
 function generateMarkdown() {
 	const marked = require('marked');
 	const rendered = marked(input.value);
@@ -85,23 +128,29 @@ function addHeadline() {
 			amoutOfLineBreaks++;
 		}
 	}
-	indexOfLastLineBreak++;
 
 	var splitTxt = text.split('\n');
 	var offset = (splitTxt[amoutOfLineBreaks].match(/#/g) || []).length;
+
+	indexOfLastLineBreak++;
+
 	if (selectionEnd === selectionStart) {
+		if(splitTxt.length === 1){
+			indexOfLastLineBreak = 0;
+		}
 		var newText =
 			text.substring(0, indexOfLastLineBreak + offset) +
 			'# ' +
 			text.substring(indexOfLastLineBreak + offset, text.length);
 	} else {
-		if (indexOfLastLineBreak > 1) {
+		console.log(indexOfLastLineBreak);
+		if (indexOfLastLineBreak > 2) {
 			var newText =
 				text.substring(0, indexOfLastLineBreak + offset) +
 				'# ' +
 				text.substring(selectionStart - 1, text.length);
 		} else {
-			var newText = text.substring(0, offset) + '# ' + text.substring(selectionStart, text.length);
+			var newText = text.substring(0, offset) + '#' + text.substring(selectionStart, text.length);
 		}
 	}
 	input.value = newText;
@@ -193,18 +242,20 @@ function highlight() {
 	var re = new RegExp(searchTerm, 'g');
 
 	if (searchTerm.length > 0) {
-		preview.innerHTML = opar.replace(re, `<mark className="found">$&</mark>`);
+		preview.innerHTML = opar.replace(re, `<mark class="found">$&</mark>`);
 	} else {
 		preview.innerHTML = opar;
 	}
 }
 
-function replace(){
+function replaceFunction() {
 	let replaceables = document.getElementsByClassName('found');
-	if(replaceables.length === 0){
+	if (replaceables.length === 0) {
 		return;
 	}
 
-	input.innerHTML.replace(replaceables, )
-
+	for (var i = 0; i < replaceables.length; i++) {
+		preview.innerHTML = preview.innerHTML.replace(replaceables[i].innerHTML, replace.value);
+		input.value = preview.textContent;
+	}
 }
